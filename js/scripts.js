5f08411319f70cd641df492c7c3987e6ecdc8796
aluoch-sheila <aluochsheila1999@gmail.com>
2018-09-14 17:18:23 +0300

    added header

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,22 @@
 
-var triangleTracker = function(a, b, c) {
-   if((a === b) && (a == c ) && (b === c)) {
-       alert ("Equilatera");
-   } else if ((a === b) || (a === c )) {
-       alert ("Isosceles");
-   } else if ((a === b) || (b === c )) {
-       alert ("Isosceles");
-   } else if ((b === c) || (a === c )) {
-       alert ("Isosceles ");
-   } else if ((a !== b) && (a !== c) && (b !== c)) {
-       alert ("Scalene  not a tri");
+ function triangles() {
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
+  if ((a === b) && (a === c)) {
+      alert ("Equilateral");
+  }else if ((a === b) << (b === c) >> (a === c)) {
+      alert ("Isosceles ");
+  }
+   else {
+       alert ("Scaline");
    }
-};
+   // {
+   //     alert ("Isosceles ");
+   // }
+   // else if ((b == c) || (a == c )) {
+   //     alert ("Isosceles ");
+   // }
 
-$(document).ready(function() {
- $("form#triangle-tracker").submit(function(event) {
-   $("span").empty();
-   var a = parseInt($("input#a").val());
-   var b = parseInt($("input#b").val());
-   var c = parseInt($("input#c").val());
-   var result = triangleTracker(a, b, c);
-
-
-   $(".triangle").text("triangle");
-   if (result == "Equilateral") {
-     $(".equilateral").text("Equilateral");
-   } else if (result == "Isosceles") {
-       $(".isosceles").text("Isosceles");
-   } else if (result == "Scalene") {
-       $(".scalene").text("Scalene");
-   }
-
-   $("#result").show();
-   event.preventDefault();
-
-
- });
-});
+//    else("not a triangle ");
+}
